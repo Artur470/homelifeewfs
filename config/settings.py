a4651bf25ab694c0968e40a9fb8e5bed1c29d8ab
Artur <arturunkulov30@gmail.com>
2024-09-16 18:19:30 +0600

    your commit

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
+ALLOWED_HOSTS = ['homelifesql.onrender.com', 'localhost', '127.0.0.1']
 
 print(f"ALLOWED_HOSTS: {ALLOWED_HOSTS}")
 print(f"SECRET_KEY: {SECRET_KEY}")
