802a5231def7bfa093541bfdc18cfc164e8763f8
Artur <arturunkulov30@gmail.com>
2024-09-19 12:19:08 +0600

    update commit rest

--- config/settings.py
+++ config/settings.py
@@ -178,18 +178,16 @@ CORS_ALLOW_CREDENTIALS = True
 
 # Настройка идентификатора сайта
 SITE_ID = 1
-
-# REST framework настройки
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_RENDERER_CLASSES': (
-        'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
+        'rest_framework.renderers.JSONRenderer',  # Оставляем только JSONRenderer
     ),
 }
+
 
 REST_AUTH = {
     'USE_JWT': True,
