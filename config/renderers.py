066ad6212a945f51395ba0a84d5dd38c74ac824a
Artur <arturunkulov30@gmail.com>
2024-09-19 13:06:52 +0600

    commit update

--- config/renderers.py
+++ config/renderers.py
@@ -1,12 +1,15 @@
-# config/renderers.py
-
 from rest_framework.renderers import JSONRenderer
 import json
 
 
 class PrettyJSONRenderer(JSONRenderer):
     def render(self, data, accepted_media_type=None, renderer_context=None):
+        # Если данные пусты, возвращаем стандартный ответ
         if data is None:
             return super().render(data, accepted_media_type, renderer_context)
 
-        return json.dumps(data, indent=4, ensure_ascii=False).encode('utf-8')
+        # Используем базовый рендерер для получения JSON-строки
+        json_data = super().render(data, accepted_media_type, renderer_context)
+
+        # Форматируем JSON с отступами
+        return json.dumps(json.loads(json_data), indent=4, ensure_ascii=False).encode('utf-8')
